51d4590f3e36ab2471a003553e81561f0f11d953
guzmanJoseph <joseph.guzman05@hotmail.com>
2026-07-09 14:26:49 -0400

    Changed wording of options in sidebar.

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -39,7 +39,7 @@ export default function Sidebar() {
                 </NavLink>
 
                 <NavLink to="/gmail-import">
-                    Gmail Import
+                    Gmail Sync
                 </NavLink>
 
                 <NavLink to="/calendar">
